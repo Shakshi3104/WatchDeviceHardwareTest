00e2de803b718c73f01ebab197a8f0655150d9c2
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-10-09 12:21:49 +0900

    Initial Commit

---
+++ WatchDeviceHardwareTest WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchDeviceHardwareTest WatchKit Extension
+//
+//  Created by MacBook Pro on 2021/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchDeviceHardwareTest WatchKit Extension/WatchDeviceHardwareTestApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchDeviceHardwareTestApp.swift
+//  WatchDeviceHardwareTest WatchKit Extension
+//
+//  Created by MacBook Pro on 2021/10/09.
+//
+
+import SwiftUI
+
+@main
+struct WatchDeviceHardwareTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
